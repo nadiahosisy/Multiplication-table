515151b37db21be9e8258bc923d5d5dcea7990e8
Nadia <nadiahosisy@gmail.com>
2023-08-24 13:16:18 +0300

    add

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ let output = "";
 for (let i = 1; i <= 10; i++) {
   output += "<tr>";
   for (let j = 1; j <= 20; j++) {
-    output += "<td>" + i * j + "</td>";
+    if (i == 1 && j == 1) {
+      output += "<th></th>";
+    } else {
+      if (i == 1 || j == 1) {
+        output += "<th>" + i * j + "</th>";
+      } else {
+        output += "<td>" + i * j + "</td>";
+      }
+    }
   }
   output += "<tr>";
 }
